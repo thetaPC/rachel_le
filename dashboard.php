854f27d5f6cd6974623e2ef9b9fa96c32cf5f79b
Maria Loza <mloza@csumb.edu>
2018-01-12 00:42:40 +0000

    renamed files to be more accurate. changed file locations for ease of acccess for Rachel

---
+++ dashboard.php
@@ -0,0 +1,102 @@
+<?php
+
+    session_start();
+    
+    if (isset($_SESSION["login"])) {  //Check whether the admin has logged in
+        if ($_SESSION["login"] == "N") {
+            header('Location: login.php');
+        }
+    } else {
+        header('Location: login.php');
+    }
+    include 'database/db_connection.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Rachel</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/social.css">
+    <link rel="stylesheet" href="css/admin/dashboard.css">
+</head>
+<body>
+  
+    <?php include 'src/repeats/navbar.php'; ?>
+  
+    <div class="container dash-section">
+        <div class="col-md-12">
+            <div class="page-header">
+                <a href=""><i class="fa fa-tachometer" aria-hidden="true"></i></a>
+                <h2>Hello, Rachel</h2>
+            </div>
+            <p class="lead"><a href="src/admin/logout.php">Log out</a></p>
+            <hr>
+            <div class="row justify-content-center m-bot">
+                <div class="col-md-3 parent">
+                    <div class="visits">
+                        <?php
+                          $sql = "SELECT total FROM visits";
+                                                
+                          $res = mysqli_query($conn, $sql);
+                          
+                          if (mysqli_num_rows($res) > 0) {
+                              while ($row = mysqli_fetch_assoc($res)) {
+                                  echo "<h1>" . $row['total'] . "</h1>";
+                              }
+                          }
+                        
+                        ?>
+                        <p>Visits</p>
+                    </div>
+                </div>
+                <div class="col-md-3 offset-md-1 parent">
+                    <div class="popular">
+                        <div class="row justify-content-md-center">
+                            <div class="col-md-5">
+                               <?php
+                                  $sql = "SELECT * FROM images WHERE clicks=(SELECT MAX(clicks) FROM images) LIMIT 1";
+                                                        
+                                  $res = mysqli_query($conn, $sql);
+                                  
+                                  if (mysqli_num_rows($res) > 0) {
+                                      while ($row = mysqli_fetch_assoc($res)) {
+                                          if ($row['clicks'] > 0) {
+                                              echo "<img class='img-thumbnail' alt='" . $row['description'] . "' src='https://res.cloudinary.com/htqimzujb/image/upload/" . $row['file_name'] . "'>" . $row['total'] . "</h>";
+                                          }
+                                      }
+                                  }
+                                
+                                ?>
+                            </div>
+                        </div>
+                        <p>Most Popular</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-2">
+                    <a href="src/admin/insert.php">Add New Image</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="src/admin/edit.php">Edit Existing Image</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="src/admin/myaccount.php">My Account</a>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
---
+++ login.php
@@ -0,0 +1,89 @@
+<?php
+    session_start();
+    include 'database/db_connection.php';
+    if (isset($_SESSION["login"])) {  //Check whether the admin has logged in
+        if ($_SESSION["login"] == "Y") {
+            header('Location: dashboard.php');
+            exit();
+        }
+    }
+    
+    // $hashed_passworddb = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+    // echo $hashed_passworddb;
+    $sql = "SELECT username, password FROM admin WHERE username='" . $_POST['username'] . "'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo $row["password"];
+        if (password_verify($_POST['pwd'], $row["password"])) {
+            $_SESSION['login'] = "Y";
+            echo "up";
+            header('Location: dashboard.php');
+            exit();
+        } else {
+            $_SESSION['login'] = "N";
+            echo "nope";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Rachel - Login</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/social.css">
+    <link rel="stylesheet" href="css/login.css" type="text/css" />
+  </head>
+
+  <body>
+
+    <?php include 'src/repeats/navbar.php'; ?>
+    <?php include 'src/repeats/social.php'; ?>
+    
+
+    <div id="login" class="login-section">
+        <div class="container login">
+            <div class="row justify-content-center">
+                <div class="col-5">
+                    <h2>Admin Login</h2>
+                </div>
+            </div>
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <form method="POST" action="">
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <input type="text" name="username" class="form-control" id="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd">Password:</label>
+                        <input type="password" name="pwd" class="form-control" id="pwd">
+                    </div>
+                    <div class="form-group row justify-content-center">
+                        <div class="col-md-3">
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        </div>
+                    </div>
+                </form> 
+            </div>
+        </div>
+        
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+    <script>
+    
+    </script>
+  </body>
+</html>
